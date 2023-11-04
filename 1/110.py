c916cdb8386c4dd06898f46787cf0e5db5986708
Darren Yang <telunyang@gmail.com>
2023-11-05 02:28:25 +0800

    不用 ** 算次方，改成 math.pow()

--- 1/110.py
+++ 1/110.py
@@ -8,7 +8,7 @@ TQC+ 程式語言Python 110 正n邊形面積計算
 請撰寫一程式，讓使用者輸入兩個正數n、s，代表正n邊形之邊長為s，計算並輸出此正n邊形之面積（Area）。
 
 提示1：建議使用import math模組的math.pow及math.tan
-提示2：正n邊形面積的公式如下：Area = (n*s**2) /(4*math.tan(math.pi/n))
+提示2：正n邊形面積的公式如下：Area = n*math.pow(s, 2))/(4*math.tan(math.pi/n))
 提示3：輸出浮點數到小數點後第四位
 
 3. 輸入輸出：
@@ -27,4 +27,7 @@ Area = 173.8234
 import math
 n = eval(input())
 s = eval(input())
-print(f'Area = {(n*s**2)/(4*math.tan(math.pi/n)):.4f}')
+print(f'Area = {(n*math.pow(s, 2))/(4*math.tan(math.pi/n)):.4f}')
+
+# 算式也可以寫成:
+# (n*s**2)/(4*math.tan(math.pi/n))
